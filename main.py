a1598d59fd15514c14b902e42f818f74f86001d4
JohnZair <johnzairoros@gmail.com>
2024-05-27 12:18:36 -0500

    Cambios en main

--- main.py
+++ main.py
@@ -3,62 +3,58 @@ import sys
 
 # Inicializar Pygame
 pygame.init()
-
 # Configuración de la pantalla
-pantalla_ancho = 1200
-pantalla_alto = 800
+pantalla_ancho = 1600
+pantalla_alto = 900
 pantalla = pygame.display.set_mode((pantalla_ancho, pantalla_alto))
 pygame.display.set_caption("Pelea de dioses")
+# Configuración del reloj (fotogramas)
+reloj = pygame.time.Clock()
 
 # Colores
 blanco = (255, 255, 255)
-negro = (0, 0, 0)
-rojo = (255, 0, 0)
-azul = (0, 0, 255)
+gris = (66, 66, 66)
+celeste = (0, 170, 228)
 amarillo = (255, 233, 0)
 naranja = (255, 165, 0)
 
-# Configuración del reloj
-reloj = pygame.time.Clock()
-
 # Plataforma
-plataforma = pygame.Rect(200, 450, pantalla_ancho-400, 200)
+plataforma = pygame.Rect(200, 550, pantalla_ancho-400, 200)
 
-# Configuración de los jugadores
+# Configuración de los jugadores (características)
 jugador1 = {
     "color": amarillo,
-    "rectan": pygame.Rect(pantalla_ancho//2-250, 400, 30, 80),
-    "velocidad": 5,
+    "rectan": pygame.Rect(pantalla_ancho//2-250, 500, 30, 80),
+    "velocidad": 6,
     "fuerza_salto": 10,
     "gravedad": 0.7,
     "velocidad_y": 0,
     "en_plataforma": False,
 }
-
 jugador2 = {
     "color": naranja,
-    "rectan": pygame.Rect(pantalla_ancho//2+220, 400, 30, 80),
-    "velocidad": 5,
+    "rectan": pygame.Rect(pantalla_ancho//2+220, 500, 30, 80),
+    "velocidad": 6,
     "fuerza_salto": 10,
     "gravedad": 0.7,
     "velocidad_y": 0,
     "en_plataforma": False,
 }
 
-def actualizar_jugador(jugador, llave, izq, derecha, salto):
-    if llave[izq]:
-        jugador["rectan"].x -= jugador["velocidad"]
-    if llave[derecha]:
-        jugador["rectan"].x += jugador["velocidad"]
-    
-    if llave[salto] and jugador["en_plataforma"]:
-        jugador["velocidad_y"] = -jugador["fuerza_salto"]
-        jugador["en_plataforma"] = False
-
+def movimiento_jugador(jugador, llave, izq, derecha, salto):
     # Aplicar gravedad
     jugador["rectan"].y += jugador["velocidad_y"]
     jugador["velocidad_y"] += jugador["gravedad"]
 
+    # Movimiento de izquierda y derecha
+    if llave[izq]:
+        jugador["rectan"].x -= jugador["velocidad"]
+    if llave[derecha]:
+        jugador["rectan"].x += jugador["velocidad"]
+    # Movimiento de salto
+    if llave[salto] and jugador["en_plataforma"]:
+        jugador["velocidad_y"] = -jugador["fuerza_salto"]
+        jugador["en_plataforma"] = False
     # Colisión con la plataforma
     if jugador["rectan"].colliderect(plataforma):
         jugador["rectan"].bottom = plataforma.top
@@ -76,14 +72,14 @@ while ejecutando:
     llaves = pygame.key.get_pressed()
 
     # Actualizar los jugadores
-    actualizar_jugador(jugador1, llaves, pygame.K_a, pygame.K_d, pygame.K_w)
-    actualizar_jugador(jugador2, llaves, pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP)
+    movimiento_jugador(jugador1, llaves, pygame.K_a, pygame.K_d, pygame.K_w)
+    movimiento_jugador(jugador2, llaves, pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP)
 
     # Dibujar todo en la pantalla
-    pantalla.fill(negro)
+    pantalla.fill(celeste)
     pygame.draw.rect(pantalla, jugador1["color"], jugador1["rectan"])
     pygame.draw.rect(pantalla, jugador2["color"], jugador2["rectan"])
-    pygame.draw.rect(pantalla, blanco, plataforma)
+    pygame.draw.rect(pantalla, gris, plataforma)
 
     # Actualizar la pantalla
     pygame.display.flip()
